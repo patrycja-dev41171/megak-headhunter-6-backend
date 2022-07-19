53052b85f008319856db6c02fba6a64f8af8b31e
Paweł Żurawka <pzurawka@gmail.com>
2022-07-19 21:39:37 +0200

    Add prettier config file.

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: false,
+  tsxSingleQuote: false,
+  arrowParens: 'avoid',
+  printWidth: 140,
+  singleAttributePerLine: true,
+};
