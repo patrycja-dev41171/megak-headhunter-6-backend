412fbb885097ddcb5df30517c91459ba96273bd2
Paweł Żurawka <pzurawka@gmail.com>
2022-07-22 17:29:39 +0200

    #19 Create user record.

---
+++ types/user/user-entity.ts
@@ -0,0 +1,9 @@
+export interface UserEntity {
+  id: string;
+  email: string;
+  courseCompletion: number;
+  courseEngagment: number;
+  projectDegree: number;
+  teamProjectDegree: number;
+  bonusProjectUrls: string;
+}
