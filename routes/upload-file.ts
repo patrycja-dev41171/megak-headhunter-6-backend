14d4eb818b41caa053e188ee63e004be5c077364
Mateusz Umięcki <mateuszumiecki@gmail.com>
2022-07-22 17:52:11 +0200

    add small validation upload.

--- routes/upload-file.ts
+++ routes/upload-file.ts
@@ -1,18 +1,32 @@
 import {Router} from 'express';
+import {ValidationError} from "../utils/handleErrors";
 
 export const uploadRouter = Router();
 
 uploadRouter
     .post('/upload', async (req, res) => {
 
-        const [data] = Object.entries(req.files);
-        const primaryData: any = data[1];
+        if (!req.files) {
+
+            throw new ValidationError('Nie przesłano pliku !');
+
+        }
+
+        try {
+            const [data] = Object.entries(req.files);
+            const primaryData: any = data[1];
 
 
-        const finallyData = [...(JSON.parse(primaryData.data))];
+            const finallyData = [...(JSON.parse(primaryData.data))];
 
-        console.log(finallyData);
+            console.log(finallyData);
+            res.send('ok');
 
-        res.send('ok');
+        } catch (err) {
+
+            throw new ValidationError("Błąd spróbuj ponownie pózniej");
+
+        }
+
 
     });
