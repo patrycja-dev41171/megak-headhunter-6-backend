2004a1ad02153387cd832d4515e97415506c4aad
Patrycja Ińska <patrycja.inska@interia.pl>
2022-07-25 12:01:44 +0200

    hr.record.ts - adding a new field: user_id after changing the database structure. Hr-entity type editing. Validation errors in hr.record.ts.

--- records/hr.record.ts
+++ records/hr.record.ts
@@ -7,39 +7,44 @@ import { pool } from '../utils/db';
 type HrRecordResult = [HrEntity[], FieldPacket[]];
 
 export class HrRecord implements HrEntity {
-  id: string;
+  id?: string;
+  user_id: string;
   email: string;
   fullName: string;
   company: string;
   maxReservedStudents: number;
 
   constructor(obj: HrEntity) {
-    if (typeof !obj.email || obj.email.length > 255) {
+    if (!obj.user_id) {
+      throw new ValidationError('User_id cannot be empty.');
+    }
+    if (!obj.email || obj.email.length > 255) {
       throw new ValidationError('Email cannot be empty and cannot exceed 255 characters.');
     }
     if (typeof obj.email !== 'string') {
       throw new ValidationError('The format of the entered data is incorrect.');
     }
-    if (typeof !obj.fullName || obj.fullName.length > 50) {
+    if (!obj.fullName || obj.fullName.length > 50) {
       throw new ValidationError('Full name cannot be empty and cannot exceed 50 characters.');
     }
     if (typeof obj.fullName !== 'string') {
       throw new ValidationError('The format of the entered data is incorrect.');
     }
-    if (typeof !obj.company || obj.company.length > 120) {
+    if (!obj.company || obj.company.length > 120) {
       throw new ValidationError('Company name cannot be empty and cannot exceed 120 characters.');
     }
     if (typeof obj.company !== 'string') {
       throw new ValidationError('The format of the entered data is incorrect.');
     }
-    if (typeof !obj.maxReservedStudents || obj.maxReservedStudents > 999) {
+    if (!obj.maxReservedStudents || obj.maxReservedStudents > 999) {
       throw new ValidationError('Number of reserved students cannot be empty and cannot exceed 999.');
     }
     if (typeof obj.maxReservedStudents !== 'number') {
       throw new ValidationError('The format of the entered data is incorrect.');
     }
 
-    this.id = obj.id;
+    this.id = obj.id ?? uuid();
+    this.user_id = obj.user_id;
     this.email = obj.email;
     this.fullName = obj.fullName;
     this.company = obj.company;
@@ -47,19 +52,10 @@ export class HrRecord implements HrEntity {
   }
 
   async insert(): Promise<void> {
-    if (!this.id) {
-      this.id = uuid();
-    }
-
-    const [results] = (await pool.execute(
-      'INSERT INTO `hr` (`id`, `email`, `fullName`, `company`, `maxReservedStudents`) VALUES (:id, ,:email :fullName, :company, :maxReservedStudents)',
-      {
-        id: this.id,
-        email: this.email,
-        fullName: this.fullName,
-        company: this.company,
-        maxReservedStudents: this.maxReservedStudents,
-      }
-    )) as HrRecordResult;
+    await pool.execute(
+      'INSERT INTO `hr` (`id`, `user_id`, `email`, `fullName`, `company`, `maxReservedStudents`)' +
+        ' VALUES (:id, :user_id ,:email :fullName, :company, :maxReservedStudents)',
+      this
+    );
   }
 }
