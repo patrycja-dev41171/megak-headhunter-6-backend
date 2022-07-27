e86700620328a8d2a440f7a7d91b2227bdf60ba2
Mateusz Umięcki <97082714+mae1579@users.noreply.github.com>
2022-07-27 19:40:39 +0200

    Add functionality students from import file, add validation email from file,modification student record, fix issues errors about add students, create function to converter array strings to json.
    Signed-off-by: Mateusz Umięcki <mateuszumiecki@gmail.com>

--- routers/upload-file.ts
+++ routers/upload-file.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import { ValidationError } from '../utils/handleErrors';
+import { UserRecord } from '../records/user.record';
+import { StudentRecord } from '../records/student.record';
+import { StudentImport } from '../types';
 
 export const uploadRouter = Router();
 
@@ -18,14 +21,53 @@ uploadRouter.post('/', async (req, res) => {
   const parseData = JSON.parse(primaryData.data);
   const emailValidate = new RegExp('@');
 
-  const validateData = parseData.map((el: any) => {
+  const validateData = parseData.map((el: StudentImport) => {
     if (!emailValidate.test(el.email)) {
       throw new ValidationError('Dane z pliku są nieprawidłowe, email musi zawierać @');
     } else {
       return el;
     }
   });
-  console.log(parseData);
+
+  for (const importStudent of validateData) {
+    const { email, courseCompletion, courseEngagement, projectDegree, teamProjectDegree, bonusProjectUrls } = importStudent;
+    if (!email) {
+      throw new ValidationError('Struktura pliku musi posiadać email !');
+    }
+    const user = {
+      email,
+      role: 'student',
+    };
+    const addUser = async () => {
+      if (user) {
+        const userTable = new UserRecord(user);
+        await userTable.insert();
+
+        const userId = await UserRecord.getOneByEmail(user.email);
+        const bonusArray = bonusProjectUrls.map((el: any) => el);
+        const correctBonus = JSON.stringify(bonusArray);
+
+        const studentImp: StudentImport = {
+          user_id: userId.id,
+          email: user.email,
+          courseCompletion,
+          courseEngagement,
+          projectDegree,
+          teamProjectDegree,
+          bonusProjectUrls: correctBonus,
+        };
+
+        try {
+          const studentAdd = new StudentRecord(studentImp);
+          await studentAdd.insert();
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    };
+
+    await addUser();
+  }
 
   // Do zrobienia: dodawanie do bazy danych oraz walidacja uzytkownikow podczas dodawania.
 
