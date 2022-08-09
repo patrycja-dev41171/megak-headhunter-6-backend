2a0cb6e0bc2ebe625fd8f986cdac2ae3a55c0189
Patrycja Ińska <patrycja.inska@interia.pl>
2022-08-09 21:59:10 +0200

    Added new endpoint hr/selected/students. Downloading from the database of reserved students, validating their reservation dates, updating and returning the list of students to the FE.

--- types/student/student-entity.ts
+++ types/student/student-entity.ts
@@ -45,6 +45,24 @@ export interface StudentGetAll {
   monthsOfCommercialExp: number;
 }
 
+export interface SelectedStudents {
+  user_id: string;
+  courseCompletion: number;
+  courseEngagement: number;
+  projectDegree: number;
+  teamProjectDegree: number;
+  firstName: string;
+  lastName: string;
+  expectedTypeWork: ExpectedTypeWork;
+  targetWorkCity: string | null;
+  expectedContractType: ExpectedContractType;
+  expectedSalary: number | null;
+  canTakeApprenticeship: number;
+  monthsOfCommercialExp: number;
+  githubUsername: string;
+  reservedTo: Date;
+}
+
 export interface SingleStudentImport {
   email: string;
   courseCompletion: number;
