4e8cd2394d5338a472fd96d250af7e378324fd36
Patrycja Ińska <patrycja.inska@interia.pl>
2022-08-10 23:34:12 +0200

    Added some changes into refreshToken.router.ts endpoint.

--- routers/refreshToken.router.ts
+++ routers/refreshToken.router.ts
@@ -6,34 +6,30 @@ import { UserRecord } from '../records/user.record';
 
 export const refreshTokenRouter = Router().get('/', async (req, res) => {
   const refreshToken: string = req.cookies.refreshToken;
-  console.log(refreshToken);
   if (!refreshToken) {
-    throw new ValidationError('Brak danych logowania w przeglądarce!');
-  }
-
-  const result = await LoginRecord.getOneByToken(refreshToken);
-  if (!result) {
-    res.clearCookie('refreshToken');
-    throw new ValidationError('Brak danych logowania w bazie danych!');
-  }
-
-  const user = await UserRecord.getOneById(result.user_id);
-
-  jwt.verify(refreshToken, process.env.ACCESS_REFRESH_TOKEN_KEY, err => {
-    if (err) {
+    res.json('');
+  } else {
+    const result = await LoginRecord.getOneByToken(refreshToken);
+    if (!result) {
       res.clearCookie('refreshToken');
-      throw new ValidationError('Nieprawidłowa weryfikacja tokenu odświeżającego!');
+      throw new ValidationError('Brak danych logowania w bazie danych!');
     }
-
-    try {
-      const accessToken = jwt.sign({ id: result.user_id }, process.env.ACCESS_TOKEN_KEY, { expiresIn: '10min' });
-      res.json({
-        accessToken: accessToken,
-        id: user.id,
-        role: user.role,
-      });
-    } catch (err) {
-      throw new ValidationError('Wystąpił błąd podczas odświeżania tokenu!');
-    }
-  });
+    const user = await UserRecord.getOneById(result.user_id);
+    jwt.verify(refreshToken, process.env.ACCESS_REFRESH_TOKEN_KEY, err => {
+      if (err) {
+        res.clearCookie('refreshToken');
+        throw new ValidationError('Nieprawidłowa weryfikacja tokenu odświeżającego!');
+      }
+      try {
+        const accessToken = jwt.sign({ id: result.user_id }, process.env.ACCESS_TOKEN_KEY, { expiresIn: '10min' });
+        res.json({
+          accessToken: accessToken,
+          id: user.id,
+          role: user.role,
+        });
+      } catch (err) {
+        throw new ValidationError('Wystąpił błąd podczas odświeżania tokenu!');
+      }
+    });
+  }
 });
